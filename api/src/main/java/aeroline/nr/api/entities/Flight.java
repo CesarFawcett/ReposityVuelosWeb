b90caef564cdc867918d6dc1bb3330c9e632fc7c
Cesar Fawcett <Cesarfawcett20@gmail.com>
2025-11-27 15:05:09 -0500

    actualizacion

--- api/src/main/java/aeroline/nr/api/entities/Flight.java
+++ api/src/main/java/aeroline/nr/api/entities/Flight.java
@@ -5,7 +5,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -13,7 +12,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name ="flights")
+@Table(name = "flights")
 @Setter
 @Getter
 @NoArgsConstructor
@@ -22,24 +21,46 @@ public class Flight {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
-    @Column(nullable = false)
+
+    @Column(name = "departure_date", nullable = false)
     private String departureDate;
-    @Column(nullable = false)
-    @JoinColumn(name = "departureAirportCode")
+
+    @Column(name = "departure_airport_code", nullable = false)
     private String departureAirportCode;
+
+    @Column(name = "departure_airport_name")
     private String departureAirportName;
+
+    @Column(name = "departure_city")
     private String departureCity;
+
+    @Column(name = "departure_locale")
     private String departureLocale;
+
+    @Column(name = "arrival_date")
     private String arrivalDate;
-    @Column(nullable = false)
+
+    @Column(name = "arrival_airport_code", nullable = false)
     private String arrivalAirportCode;
+
+    @Column(name = "arrival_airport_name")
     private String arrivalAirportName;
+
+    @Column(name = "arrival_city")
     private String arrivalCity;
+
+    @Column(name = "arrival_locale")
     private String arrivalLocale;
+
+    @Column(name = "ticket_price")
     private int ticketPrice;
+
+    @Column(name = "ticket_currency")
     private String ticketCurrency;
+
+    @Column(name = "flight_number")
     private int flightNumber;
-    private int seatCapacity; 
-   // @OneToMany(mappedBy = "outboundFlight")
-    //private List<Booking> bookings = new ArrayList<>();
+
+    @Column(name = "seat_capacity")
+    private int seatCapacity;
 }
